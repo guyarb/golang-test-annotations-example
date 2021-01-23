82d4a61300c6607e1324aaafc0d63a487a447007
Guy Arbitman <guy20495@gmail.com>
2021-01-23 07:01:06 +0200

    Added an example usage for golang-test-annotations actions

---
+++ foo_test.go
@@ -0,0 +1,15 @@
+package foo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_foo_failed(t *testing.T) {
+	require.Equal(t, 0, 1)
+}
+
+func Test_goo_succeeded(t *testing.T) {
+	require.Equal(t, 0, 0)
+}
